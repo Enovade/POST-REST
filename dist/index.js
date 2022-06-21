63408a6eae58a30ebbefb019bfd7dadb8b1f3d49
Hanafiah <hanafiah@cloud-connect.asia>
2022-06-22 01:19:15 +0800

    modify output

--- dist/index.js
+++ dist/index.js
@@ -8291,8 +8291,8 @@ const url = core.getInput('url', { required: true });
 axios.get(url)
 .then(res => {
     console.log(res.data)
-    //core.setOutput('jawapan', res.data);
-    core.setOutput('jawapan', 'cubaan');
+    core.setOutput('jawapan', res.data);
+    //core.setOutput('jawapan', 'cubaan');
 })
 .catch(err => {
     console.error(err); 
--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const url = core.getInput('url', { required: true });
 axios.get(url)
 .then(res => {
     console.log(res.data)
-    //core.setOutput('jawapan', res.data);
-    core.setOutput('jawapan', 'cubaan');
+    core.setOutput('jawapan', res.data);
+    //core.setOutput('jawapan', 'cubaan');
 })
 .catch(err => {
     console.error(err); 
