e099036552076491ffcc4b335521e0afdc951e15
Hanafiah <hanafiah@cloud-connect.asia>
2022-06-21 16:40:13 +0800

    install github core

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const axios = require('axios');
+const core = require('@actions/core');
+//const github = require('@actions/github');
 
-const url = "http://202.165.24.140:1337/jpns?jenis=pengumuman"
+const url = core.getInput('url', { required: true });
+//const url = "http://202.165.24.140:1337/jpns?jenis=pengumuman"
 //axios.get(url,params)
 axios.get(url)
 .then(res => {
